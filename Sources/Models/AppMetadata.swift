eb7fbb170ec268ca884f847ce9557de4acc5c3f7
Seshak-code <seslak@outlook.com>
2026-05-24 20:00:55 -0500

    Add AppMetadata model

---
+++ Sources/Models/AppMetadata.swift
@@ -0,0 +1,11 @@
+import Foundation
+import AppKit
+
+struct AppMetadata {
+    var title: String
+    var description: String
+    var imageURL: String?
+    var cachedImage: NSImage?
+    var backdropURL: String?
+    var cachedBackdrop: NSImage?
+}
